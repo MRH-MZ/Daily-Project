93ef5a98256536bb37a1a90b09118a112498ce94
MRH <github.nexnith@gmail.com>
2026-09-07 00:17:13 -0800

    Complete File Handling project

--- scores.py
+++ scores.py
@@ -1,5 +1,35 @@
 file = open("scores.txt")
 file_read = file.readlines()
-for i in file_read:
-    file_int = int(i)
-    print(file_int)
+
+len_file = 0
+sum_number  = 0
+file_list = []
+
+
+for j in file_read:
+    i = int(j)
+    len_file += 1
+    sum_number += i
+    file_list.append(i)
+
+
+Min_number = file_list[0]
+Max_number = 0
+
+
+for i in file_list:
+    if Max_number < i :
+        Max_number = i
+    if Min_number > i :
+        Min_number = i 
+avrage = sum_number / len_file
+
+
+file = open ("result_scores.txt","w")
+file.writelines([f"Total = {sum_number}\n"
+                    f"Avrage = {avrage}\n"
+                    f"Max = {Max_number}\n"
+                    f"Min = {Min_number}\n"
+                    ])
+
+file.close()
