ae19bb2f9ae6965234733c15b94d2773caee1631
MRH <github.nexnith@gmail.com>
2026-09-09 01:23:10 -0800

    complete Operators 2

---
+++ operators_2.py
@@ -0,0 +1,5 @@
+Number_1 = int(input("Enter the number 1 : "))
+Number_2 = int(input("Enter the number 2 : "))
+Number_3 = int(input("Enter the number 3 : "))
+result = Number_1 < Number_2 <= Number_3
+print(result)
